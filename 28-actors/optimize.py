687e8b4b74916ea42cf9918a709ca70e7e65619c
AngelLiang <pl01665077@163.com>
2018-08-25 17:42:50 +0800

    update

--- 28-actors/optimize.py
+++ 28-actors/optimize.py
@@ -2,7 +2,9 @@
 # coding=utf-8
 """
 tf-28.py优化
-使Python2和Python3都可以运行
+    1、使Python2和Python3都可以运行；
+    2、修改被调度的方法的名称，使之拥有`on_`前缀；
+    3、修改调度方式，使用`getattr`获取当前类的接口。
 """
 
 import sys
@@ -11,13 +13,7 @@ import operator
 import string
 from threading import Thread, Event
 
-try:
-    import six
-except:
-    print("please install six")
-    print("pip install six")
-    exit(1)
-
+import six
 from six.moves.queue import Queue
 
 
@@ -47,16 +43,24 @@ class DataStorageManager(ActiveWFObject):
     """ Models the contents of the file """
     _data = ''
 
+    _dispatch_functions = ["init", "send_word_freqs"]
+
     def _dispatch(self, message):
-        if message[0] == 'init':
-            self._init(message[1:])
-        elif message[0] == 'send_word_freqs':
-            self._process_words(message[1:])
+        # if message[0] == 'init':
+        #     self.on_init(message[1:])
+        # elif message[0] == 'send_word_freqs':
+        #     self.on_process_words(message[1:])
+        # else:
+        #     # forward
+        #     self._stop_word_manager.send(message)
+
+        if message[0] in self._dispatch_functions:
+            getattr(self, 'on_' + message[0])(message[1:])
         else:
             # forward
             self._stop_word_manager.send(message)
 
-    def _init(self, message):
+    def on_init(self, message):
         path_to_file = message[0]
         self._stop_word_manager = message[1]
         with open(path_to_file) as f:
@@ -64,7 +68,7 @@ class DataStorageManager(ActiveWFObject):
         pattern = re.compile('[\W_]+')
         self._data = pattern.sub(' ', self._data).lower()
 
-    def _process_words(self, message):
+    def on_process_words(self, message):
         recipient = message[0]
         data_str = ''.join(self._data)
         words = data_str.split()
@@ -72,27 +76,38 @@ class DataStorageManager(ActiveWFObject):
             self._stop_word_manager.send(['filter', w])
         self._stop_word_manager.send(['top25', recipient])
 
+    def on_send_word_freqs(self, message):
+        self.on_process_words(message)
+
 
 class StopWordManager(ActiveWFObject):
     """ Models the stop word filter """
     _stop_words = []
 
+    _dispatch_functions = ["init", "filter"]
+
     def _dispatch(self, message):
-        if message[0] == 'init':
-            self._init(message[1:])
-        elif message[0] == 'filter':
-            return self._filter(message[1:])
+        # if message[0] == 'init':
+        #     self.on_init(message[1:])
+        # elif message[0] == 'filter':
+        #     return self.on_filter(message[1:])
+        # else:
+        #     # forward
+        #     self._word_freqs_manager.send(message)
+
+        if message[0] in self._dispatch_functions:
+            getattr(self, 'on_' + message[0])(message[1:])
         else:
             # forward
             self._word_freqs_manager.send(message)
 
-    def _init(self, message):
+    def on_init(self, message):
         with open('../stop_words.txt') as f:
             self._stop_words = f.read().split(',')
         self._stop_words.extend(list(string.ascii_lowercase))
         self._word_freqs_manager = message[0]
 
-    def _filter(self, message):
+    def on_filter(self, message):
         word = message[0]
         if word not in self._stop_words:
             self._word_freqs_manager.send(['word', word])
@@ -102,20 +117,28 @@ class WordFrequencyManager(ActiveWFObject):
     """ Keeps the word frequency data """
     _word_freqs = {}
 
-    def _dispatch(self, message):
-        if message[0] == 'word':
-            self._increment_count(message[1:])
-        elif message[0] == 'top25':
-            self._top25(message[1:])
+    _dispatch_functions = ["word", "top25"]
 
-    def _increment_count(self, message):
+    def _dispatch(self, message):
+        # if message[0] == 'word':
+        #     self.on_increment_count(message[1:])
+        # elif message[0] == 'top25':
+        #     self.on_top25(message[1:])
+
+        if message[0] in self._dispatch_functions:
+            getattr(self, 'on_' + message[0])(message[1:])
+
+    def on_increment_count(self, message):
         word = message[0]
         if word in self._word_freqs:
             self._word_freqs[word] += 1
         else:
             self._word_freqs[word] = 1
 
-    def _top25(self, message):
+    def on_word(self, message):
+        self.on_increment_count(message)
+
+    def on_top25(self, message):
         recipient = message[0]
 
         # freqs_sorted = sorted(self._word_freqs.iteritems(),
@@ -127,19 +150,26 @@ class WordFrequencyManager(ActiveWFObject):
 
 class WordFrequencyController(ActiveWFObject):
 
+    _dispatch_functions = ["run", "top25"]
+
     def _dispatch(self, message):
-        if message[0] == 'run':
-            self._run(message[1:])
-        elif message[0] == 'top25':
-            self._display(message[1:])
+        # if message[0] == 'run':
+        #     self.on_run(message[1:])
+        # elif message[0] == 'top25':
+        #     self.on_display(message[1:])
+        # else:
+        #     raise Exception("Message not understood " + message[0])
+
+        if message[0] in self._dispatch_functions:
+            getattr(self, 'on_' + message[0])(message[1:])
         else:
             raise Exception("Message not understood " + message[0])
 
-    def _run(self, message):
+    def on_run(self, message):
         self._storage_manager = message[0]
         self._storage_manager.send(['send_word_freqs', self])
 
-    def _display(self, message):
+    def on_display(self, message):
         word_freqs = message[0]
         for (w, f) in word_freqs[0:25]:
             print(str(w)+' - '+str(f))
@@ -147,20 +177,35 @@ class WordFrequencyController(ActiveWFObject):
         # self._stop_evt = True
         self._stop_evt.set()
 
+    def on_top25(self, message):
+        self.on_display(message)
 
-#
-# The main function
-#
-word_freq_manager = WordFrequencyManager()
 
-stop_word_manager = StopWordManager()
-stop_word_manager.send(['init', word_freq_manager])
+def main():
+    #
+    # The main function
+    #
+    word_freq_manager = WordFrequencyManager()
 
-storage_manager = DataStorageManager()
-storage_manager.send(['init', sys.argv[1], stop_word_manager])
+    stop_word_manager = StopWordManager()
+    stop_word_manager.send(['init', word_freq_manager])
 
-wfcontroller = WordFrequencyController()
-wfcontroller.send(['run', storage_manager])
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+    else:
+        arg = "../input.txt"
+    print("arg = '%s'" % arg)
 
-# Wait for the active objects to finish
-[t.join() for t in [word_freq_manager, stop_word_manager, storage_manager, wfcontroller]]
+    storage_manager = DataStorageManager()
+    storage_manager.send(['init', arg, stop_word_manager])
+
+    wfcontroller = WordFrequencyController()
+    wfcontroller.send(['run', storage_manager])
+
+    # Wait for the active objects to finish
+    [t.join() for t in [word_freq_manager, stop_word_manager,
+                        storage_manager, wfcontroller]]
+
+
+if __name__ == '__main__':
+    main()
